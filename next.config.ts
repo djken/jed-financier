e51510440fab0b54c461b23bd1d610734a2c55e2
Jean-Kenel <jeankeneld@gmail.com>
2025-11-19 19:16:21 -0500

    update 02

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    outputStandalone: true,
-  },
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-module.exports = nextConfig;
+export default nextConfig;
+
